436a58204f3f95a3aafe2d1959e98cf7ca091e12
j.c. <jcbackenddev@gmail.com>
2020-06-16 17:38:56 +0200

    Changes to connect with local DB
    The Driver had to be updated to connect to the local host on my
    computer.

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,19 +1,41 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
+
+import junit.framework.Assert;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -44,19 +66,47 @@ public class ParkingDataBaseIT {
 
     @AfterAll
     private static void tearDown(){
-
+    	//TODO: should this function delete the data created in the tests?
     }
-
+    
     @Test
-    public void testParkingACar(){
+    public void testParkingACar() throws Exception{
+    	//ARRANGE
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        String testRegistrationNumber = "ABCDEF";
+        
+        // ACT
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        
+        // ASSERT
+        assertEquals(ticket.getVehicleRegNumber(), testRegistrationNumber);
+        
+        //TODO: check that a ticket is actually saved in DB and Parking table is updated with availability
     }
-
+    
     @Test
-    public void testParkingLotExit(){
-        testParkingACar();
+    public void testParkingTableAvailability() throws Exception{
+    	//ARRANGE
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        
+        // ACT
+        parkingService.processIncomingVehicle();
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+
+        // ASSERT
+        assertEquals(ticket.getParkingSpot().isAvailable(), false);
+        //TODO: check that a ticket is actually saved in DB and Parking table is updated with availability
+    }
+    
+    @Test
+    public void testParkingLotExit() throws ClassNotFoundException{
+        try {
+			testParkingACar();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
